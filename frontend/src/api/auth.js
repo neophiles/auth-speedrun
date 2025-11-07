426fde9ab32d8c655f17e16d8a62748ca4094a71
neophiles <mnv.tagle@gmail.com>
2025-11-07 21:25:13 +0800

    feat: add login function

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -5,3 +5,17 @@ export const register = async (data) => {
   console.log(res.data) 
   return res.data;
 };
+
+export const login = async (data) => {
+  const formData = new URLSearchParams();
+  formData.append("grant_type", "password");
+  formData.append("username", data.username);
+  formData.append("password", data.password);
+
+  const res = await api.post('/auth/token', formData, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+  return res.data;
+};
