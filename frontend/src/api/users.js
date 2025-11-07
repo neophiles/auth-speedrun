fa41cd3a7109b4d7be5e16068ed9e9e88477e85b
neophiles <mnv.tagle@gmail.com>
2025-11-08 04:36:34 +0800

    fix: change path to /auth/me

--- frontend/src/api/users.js
+++ frontend/src/api/users.js
@@ -1,7 +1,7 @@
 import api from './axios';
 
 export const getUser = async (token) => {
-  const res = await api.get("/users/me", {
+  const res = await api.get("/auth/me", {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res.data;
