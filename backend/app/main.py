8080db2abb5e13cb1a2136249c3e0274a83e5343
eigenlambda123 <rmvilla987@gmail.com>
2025-11-07 20:28:33 +0800

    feat: database setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.db.database import init_db
 
 app = FastAPI()
+
+# CORS Setup
+origins = [
+    "http://localhost:5173", 
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
 
 @app.get("/")
 def read_root():
