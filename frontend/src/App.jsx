d096d1c220c0fc8b19ab25ad1808d9f88891d81c
neophiles <mnv.tagle@gmail.com>
2025-11-07 21:03:08 +0800

    chore: comment out other routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider, useAuth } from "./context/AuthProvider";
+import Register from './pages/Register';
 
-function App() {
-  const [count, setCount] = useState(0)
+function AppRoutes() {
+  const { authToken } = useAuth();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      {!authToken ? (
+        <>
+          {/* <Route path="/login" element={<Login />} /> */}
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Navigate to="/register" replace />} />
+        </>
+      ) : (
+        <>
+          {/* <Route path="/" element={<RootLayout />}>
+            <Route index element={<Page1 />} />
+            <Route path="page1" element={<Page1 />} />
+            <Route path="page2" element={<Page2 />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/page1" replace />} /> */}
+        </>
+      )}
+    </Routes>
+  );
 }
 
-export default App
+export default function App() {
+  return (
+     <AuthProvider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
